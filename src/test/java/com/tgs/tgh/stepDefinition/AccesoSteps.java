0c2c52ff2b4662857a91ce643e79a296dc738bec
JaimePerezP <43539902+JaimePerezP@users.noreply.github.com>
2019-10-17 14:16:57 +0200

    prueba travis driver chrome

--- src/test/java/com/tgs/tgh/stepDefinition/AccesoSteps.java
+++ src/test/java/com/tgs/tgh/stepDefinition/AccesoSteps.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
