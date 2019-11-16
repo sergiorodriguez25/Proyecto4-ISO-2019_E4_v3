d56c5edd969e47396e5dee4a75b74b64d0ea5c7f
JaimePerezP <43539902+JaimePerezP@users.noreply.github.com>
2019-11-16 12:33:48 +0100

    3.0.7
    Definición de pruebas del grupo médico

---
+++ src/test/java/com/tgs/tgh/stepDefinition/GrupoMedicoSteps.java
@@ -0,0 +1,21 @@
+package com.tgs.tgh.stepDefinition;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class GrupoMedicoSteps {
+	
+	@When("^Se reciben los datos del grupo medico$")
+	public void se_reciben_los_datos_del_grupo_medico() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Se le asigna el grupo medico al paciente$")
+	public void se_le_asigna_el_grupo_medico_al_paciente() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
